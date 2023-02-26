5c4e3bf7548cac218aef966f19125c7a2312482e
aletondaX <gtrrsta@outlook.com>
2023-02-26 00:00:53 -0300

    métodos re-hechos con inner

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,27 @@
-document.body.onload = agregarCards;
+function agregarCardsConInner() {
+    let cards = [];
+    let card;
+    for (let evento of data.events) {
+        card = `<div class="col">
+                    <div class="card">
+                        <img src="${evento.image}" class="card-img-top" alt="${evento.name} Image">
+                        <div class="card-body">
+                            <h5 class="card-title">${evento.name}</h5>
+                            <p class="card-text">${evento.description}</p>
+                            <div class="d-flex justify-content-between">
+                                <p>Price: $${evento.price}</p>
+                                <a href="./details.html">see more...</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
+        cards.push(card);
+    }
+    document.getElementById("insertar-cards").innerHTML = cards.join("");
+}
 
-function agregarCards() {
-    // console.log(data.events.length);
-
+//Opcional
+function agregarCardsConAppend() {
     for (let evento of data.events) {
         let card = document.createElement("div");
         card.setAttribute("class", "col");
@@ -20,3 +39,6 @@ function agregarCards() {
         document.getElementById("insertar-cards").appendChild(card);
     }
 }
+
+agregarCardsConInner();
+// agregarCardsConAppend();
--- scripts/past.js
+++ scripts/past.js
@@ -1,15 +1,43 @@
-document.body.onload = agregarCards;
-
-function agregarCards() {
-    // console.log(data.events.length);
+function agregarCardsConInner() {
+    let cards = [];
+    let card;
     let fechaActual = new Date(data.currentDate);
     console.log("Fecha Actual: " + fechaActual.toLocaleDateString());
-
+    console.log("#########################");
     for (let evento of data.events) {
         let fechaEvento = new Date(evento.date);
-        console.log("Fecha Evento: " + fechaEvento.toLocaleDateString());
+        console.log("Fecha Evento nº" + evento._id + ": " + fechaEvento.toLocaleDateString());
         if (fechaActual > fechaEvento) {
-            console.log("<< Es evento PASADO >>");
+            console.log("( Es evento PASADO )");
+            card = `<div class="col">
+                    <div class="card">
+                        <img src="${evento.image}" class="card-img-top" alt="${evento.name} Image">
+                        <div class="card-body">
+                            <h5 class="card-title">${evento.name}</h5>
+                            <p class="card-text">${evento.description}</p>
+                            <div class="d-flex justify-content-between">
+                                <p>Price: $${evento.price}</p>
+                                <a href="./details.html">see more...</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
+            cards.push(card);
+        }
+    }
+    document.getElementById("insertar-cards").innerHTML = cards.join("");
+}
+
+//Opcional
+function agregarCardsConAppend() {
+    let fechaActual = new Date(data.currentDate);
+    console.log("Fecha Actual: " + fechaActual.toLocaleDateString());
+    console.log("#########################");
+    for (let evento of data.events) {
+        let fechaEvento = new Date(evento.date);
+        console.log("Fecha Evento nº" + evento._id + ": " + fechaEvento.toLocaleDateString());
+        if (fechaActual > fechaEvento) {
+            console.log("( Es evento PASADO )");
             let card = document.createElement("div");
             card.setAttribute("class", "col");
             card.innerHTML = `<div class="card">
@@ -27,3 +55,6 @@ function agregarCards() {
         }
     }
 }
+
+agregarCardsConInner();
+// agregarCardsConAppend();
