8d877ebb12a43a96a749247c252f658c1ebde909
aletondaX <gtrrsta@outlook.com>
2023-03-06 19:26:42 -0300

    esaaa

--- scripts/rendercards.js
+++ scripts/rendercards.js
@@ -4,31 +4,32 @@ function renderCards(eventosFiltrados, inputText) {
         let card;
         for (let evento of eventosFiltrados) {
             card = `<div class="col">
-                    <div class="card">
-                        <img src="${evento.image}" class="card-img-top" alt="${evento.name} Image">
-                        <div class="card-body">
-                            <h5 class="card-title">${evento.name}</h5>
-                            <p class="card-text">${evento.description}</p>
-                            <div class="d-flex justify-content-between">
-                                <p>Price: $${evento.price}</p>
-                                <a href="./details.html?id=${evento._id}">View Details</a>
+                        <div class="card">
+                            <img src="${evento.image}" class="card-img-top" alt="${evento.name} Image">
+                            <div class="card-body">
+                                <h5 class="card-title">${evento.name}</h5>
+                                <p class="card-text">${evento.description}</p>
+                                <div class="d-flex justify-content-between">
+                                    <p>Price: $${evento.price}</p>
+                                    <a href="./details.html?id=${evento._id}">View Details</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>`;
+                    </div>`;
             cards.push(card);
         }
         document.getElementById("insertar-cards").innerHTML = cards.join("");
     } else {
-        document.getElementById("insertar-cards").innerHTML = `<div class="col">
-        <div class="card">
-            <img src="./img/notfound.jpg" class="card-img-top" alt="Event Not Found">
-            <div class="card-body">
-                <h5 class="card-title">"${inputText}"? Not Found!</h5>
-                <p class="card-text">The event you are looking for was not found!</p>
+        document.getElementById("insertar-cards").innerHTML = 
+        `<div class="col">
+            <div class="card">
+                <img src="./img/notfound.jpg" class="card-img-top" alt="Event Not Found">
+                <div class="card-body">
+                    <h5 class="card-title">"${inputText}"? Not Found!</h5>
+                    <p class="card-text">The event you are looking for was not found!</p>
+                </div>
             </div>
-        </div>
-    </div>`;
+        </div>`;
     }
 }
 
