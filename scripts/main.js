e633add7e9c0c7c369a004b286f3bd7fc2984075
aletondaX <gtrrsta@outlook.com>
2023-03-07 23:04:24 -0300

    agregue algo

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
 function filtrar() {
     let inputText = document.getElementById("search-input").value.trim();
     let checks = Array.from(document.querySelectorAll(".categoria:checked")).map(check => check.value);
+    console.log(document.querySelectorAll(".categoria:checked"));
+    // console.log(Array.from(document.querySelectorAll(".categoria:checked")));
+    // console.log(Array.from(document.querySelectorAll(".categoria:checked")).map(check => check.value));
 
     let eventosFiltrados = data.events.filter(evento => {
         return (evento.name.toLowerCase().includes(inputText.toLowerCase()) && (checks.length === 0 || checks.includes(evento.category)))
